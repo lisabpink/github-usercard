79145be94b3b0b4773c487f1eb48a14bbc743baa
LISA_COOK <lisabpink@gmail.com>
2019-12-05 18:03:20 -0500

    Spet 1-3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-  
+
+axios.get('https://api.github.com/users/lisabpink')
+  .then (data => {
+    console.log('data: ', data)
+    const myInfo = data.data;
+    console.log('UserInfo: ', myInfo)
+    
+
+   
+
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +35,10 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+ 
+];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
